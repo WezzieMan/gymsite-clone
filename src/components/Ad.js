d878fca7d3b7401c3cd2a90833fb492ad7480111
Daniel Marcoci <florian.danutz15@gmail.com>
2022-12-05 00:32:50 +0100

    clone finished. added last part.

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -1,7 +1,9 @@
+import ad from '../assets/ad.jpg'
+
 const Ad = () => {
   return (  
-    <div>
-      
+    <div className='max-w-[80%] mx-auto my-[1.5rem]'>
+      <img src={ad} alt="" className='items-center'/>
     </div>
   );
 }
